4e42198ba2eb60f5c0a7df612979c400faf7e374
Nattawut Visuttipinat <31764710+paoonline@users.noreply.github.com>
2025-06-11 20:27:06 +0700

    add kafka websocket

--- src/notification/application/notification.worker.ts
+++ src/notification/application/notification.worker.ts
@@ -32,3 +32,46 @@ export class NotificationWorker extends BaseWorker implements OnModuleInit {
     console.log('👷 NotificationWorker initialized');
   }
 }
+
+
+// import { Inject, Injectable, OnModuleInit } from "@nestjs/common";
+// import Redis from "ioredis";
+// import { CustomerRepository } from "src/customer/infrastructure/customer.repository";
+// import { REDIS_CLIENT } from "src/services/Redis/redis.module";
+// import { BaseWorker } from "src/services/Worker/BaseWorker";
+// import { NotificationRepository } from "../infrastructure/notification.repository";
+// import { ConsultNotification } from "@prisma/client";
+// import { NotificationService } from "./notification.service";
+// import { ConsultNotificationDto } from "src/consult/application/dto/consult.noti.dto";
+// import { KafkaService } from "src/services/Kafka/kafka.service";
+
+// @Injectable()
+// export class NotificationWorker implements OnModuleInit {
+//   constructor(
+
+//     private readonly notificationService: NotificationService,
+//     private readonly kafkaService: KafkaService,
+//   ) {
+//     // super('NotificationQueue', redis, async (job) => {
+//     //   const { id, description, title, device_token } = job.data;
+//     // });
+//   }
+
+//   async onModuleInit() {
+//     await this.kafkaService.consumeMessages('NotificationQueue', (msg) => {
+//       console.log('Received:', msg);
+//       let msgData = msg
+
+//       let data = {
+//         consultTransactionId: msgData.id,
+//         description: msgData.description,
+//         title: msgData.title,
+//         deviceToken: msgData.device_token
+//     }  as ConsultNotificationDto
+//     this.notificationService.create(data)
+  
+//     });
+//     // Optionally log something
+//     console.log('👷 NotificationWorker initialized');
+//   }
+// }
